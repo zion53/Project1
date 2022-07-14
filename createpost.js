4633572ec67c0281570df272953234761facb297
zion53 <99487806+zion53@users.noreply.github.com>
2022-07-14 23:28:25 +0530

    Update createpost.js

--- createpost.js
+++ createpost.js
@@ -5,7 +5,7 @@ const getloc=()=>{
         console.log(position);
         const lat=position.coords.latitude;
         const longi=position.coords.longitude;
-        const getAPI=`https://us1.locationiq.com/v1/reverse.php?key=pk.b7a52b506b81aa06d13b171739585e55&lat=${lat}&lon=${longi}&format=json`
+        const getAPI=`https://us1.locationiq.com/v1/reverse.php?key=pk.55bb9ec29a7062d89a11137fd5f7878f&lat=${lat}&lon=${longi}&format=json`
         fetch(getAPI).then(res => res.json())
         .then(data =>{
             console.log(data);
